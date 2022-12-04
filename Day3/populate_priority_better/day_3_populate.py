10fe2ca5fca939d9bcb5e8e64deacce8ffadae2d
mnvu42 <mnvu42@tntech.edu>
2022-12-03 23:57:36 -0600

    Resolved issue

--- Day3/populate_priority_better/day_3_populate.py
+++ Day3/populate_priority_better/day_3_populate.py
@@ -1,2 +1,15 @@
 # TODO: How can I populate the priority dictionary faster?
 # code
+
+def populate_priority(c : str) -> int:
+    score = 0
+    c = c[0]
+    if c.isupper():
+        score += ord(c) - ord('A') + 27
+    else:
+        score += ord(c) - ord('a') + 1
+
+    return score
+
+if __name__ == '__main__':
+    print(populate_priority('rr'))
